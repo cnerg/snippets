74d9d696146a67d72bea2c2576f1feb52d8f38de
Baptiste Mouginot <mouginot.baptiste@gmail.com>
2019-10-24 15:45:00 -0500

    add operation and test for it

---
+++ source/calc.py
@@ -0,0 +1,13 @@
+import math
+
+def artih_op(a, b, op):
+    if op == "+":
+        c = a+b
+    else if c == '-':
+        c = a-b
+    else:
+        raise NotImplementedError
+    return c
+
+def circle_area(r):
+    return math.pi*r**2
---
+++ tests/test_calc.py
@@ -0,0 +1,41 @@
+import pytest
+
+import cacl as cc
+
+class TestArithOp(object):
+
+
+    def test_add_simple(self):
+        obs = cc.arith_op(3,2, '+')
+        assert 5 == obs
+
+
+    @pytest.mark.parametrize("n1, n2, op, exp", [
+        (9, 2, '+', 11),
+        (19, 2, '-', 17),
+        ])
+    def test_sub_multi(self, n1, n2, op, exp):
+        obs = cc.arith_op(n1, n2, op)
+        assert exp == obs
+
+    def test_type_error(self):
+        with pytest.raises(TypeError):
+            cc.arith_op('3',1,'+')
+    
+    def test_type_Implementation(self):
+        with pytest.raises(NotImplementedError):
+            cc.arith_op('3',1,'/')
+    
+    @pytest.mark.parametrize("n1, n2, op, exp_error", [
+        ('9', 2, '+',TypeError),
+        (19, 2, '/', NotImplementedError),
+        ])
+    def test_par_error(self, n1, n2, op, exp_error):
+        with pytest.raises(exp_err):
+            cc.arith_op(n1,n2,op)
+
+class TestCircleArea(object):
+    
+    def test_approx(self):
+        obs = cc.circle_area(1)
+        assert obs == pytest.approx(3.1415, rel=1e-6)
